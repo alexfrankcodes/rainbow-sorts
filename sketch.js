fd495abbf05bdea7cbd6094553b21d7f25a7066b
Alex Frank <alexfrankcodes@gmail.com>
2021-01-21 10:29:10 -0800

    Fixed sorting order for selection sort visualization

--- sketch.js
+++ sketch.js
@@ -175,13 +175,13 @@ async function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[min]) {
+      if (arr[j] > arr[min]) {
         min = j;
       }
     }
     if (min != i) {
       await sleep();
-      await swap(arr, min, i);
+      await swap(arr, i, min);
     }
   }
   return arr;
